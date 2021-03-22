a9f4ead94c1332b101afc82c77f51d39768e24c2
Leon Markovich <leonmarkovich97@gmail.com>
2021-03-22 19:31:39 +0200

    finished log round

--- src/Log.cpp
+++ src/Log.cpp
@@ -6,8 +6,14 @@
 Log::Log() : Function("ln(x)"), ln_base(1) {}
 
 //equation is log_N(x)
-Log::Log(double num) : Function("log_" + std::to_string(num) + "(x)"), ln_base(std::log(num)) {}
+Log::Log(double num) : Function(set_logString(num)), ln_base(std::log(num)) {}
 
+//gets an equation for base of log and changed to string , after a talk with michal 
+//she said that we should alwayes round 2 numbers after the dot ..
+std::string Log::set_logString(double num) {
+	auto temp = std::to_string(num);
+	return "log_" + temp.substr(0, temp.find('.') + 3) + "(x)";
+}
 //return log of x in the given base
 double Log::calculate(double x) const{
 	return std::log(x)/ln_base;
