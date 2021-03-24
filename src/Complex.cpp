c406cc81ad033e72275db26e69eb31bb891d61fd
Leon Markovich <leonmarkovich97@gmail.com>
2021-03-24 16:08:44 +0200

    removed unneccery get()..

--- src/Complex.cpp
+++ src/Complex.cpp
@@ -3,11 +3,11 @@ Complex::Complex(const std::shared_ptr<Function>& left, const std::shared_ptr<Fu
 	: m_operandL(left), m_operandR(right), Function(equation) {
 }
 double Complex::calculate_right(double x)const{
-	return m_operandR.get()->calculate(x);
+	return m_operandR->calculate(x);
 	
 }
 
 //returns the calculation of the second/left part of the complex function
 double Complex::calculate_left(double x)const {
-	return m_operandL.get()->calculate(x);
+	return m_operandL->calculate(x);
 }
