a38d4e8d2dd7230323f0427e1c57b53bf90403a3
ronehe <Ro.Heifetz@gmail.com>
2021-03-28 21:06:38 +0300

    added notes on assign function and on function.h

--- include/Function.h
+++ include/Function.h
@@ -12,8 +12,8 @@ private:
 public:
 	Function(std::string equation);
 	double virtual calculate(double x) const = 0;
-	void print() const;
-	void print(double) const;
+	void print() const; //prints equation with 'x' as variable
+	void print(double) const; //prints equation with real value as variable
 	std::string virtual get_equation() const;
-	std::string assign(std::string)const;
+	std::string assign(std::string)const; //create equation evaluated at some point/function
 };
