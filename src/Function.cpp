46d09064f8774c8e8b19a110c8d0369d30fc77a0
ronehe <Ro.Heifetz@gmail.com>
2021-03-28 21:16:06 +0300

    added notes on assign function on readme branch

--- src/Function.cpp
+++ src/Function.cpp
@@ -16,18 +16,27 @@ std::string Function::get_equation() const {
 	return m_equation;
 }
 
+//this function is for generating a new equation of a function with a different varaible instead of x
+//we are using it using the tools from the standard library: 
+//it is possible to decompose the string into samples which have the character 'x' in between them.
+//we generate those samples using substr function, and "squeeze" in between each sample the wanted string
+//to be put instead of x. used mainly on Compose_Function to create a string of composing functions
+//and printing evaluatin at a given point.
+
+//input: the wanted variable
+//output: the equation of the function with the input variable
 std::string Function::assign(std::string value) const
 {
-	std::string return_string{}; //return value
+	std::string return_string{}; //output string
 	auto x_pos = m_equation.find('x');
-	size_t total_pos = 0;
+	size_t total_pos = 0; //index in equation
 	std::string temp{};
 	int i = 0;
 	while (x_pos != std::string::npos) {
-		temp = m_equation.substr(total_pos , x_pos);
+		temp = m_equation.substr(total_pos, x_pos); //create sample in between 2 'x' characters, or beginning of string with 'x'
 		return_string += temp + value;
 		total_pos += x_pos + 1;
-		x_pos = m_equation.substr(total_pos).find('x');
+		x_pos = m_equation.substr(total_pos).find('x'); //find next 'x' to create next sample
 	}
-	return return_string + m_equation.substr(total_pos);
+	return return_string + m_equation.substr(total_pos); //add the remainder until end
 }
