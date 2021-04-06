d3f4fcdb6545b85337d506ffc8562d7bc96a4c92
Leon Markovich <leonmarkovich97@gmail.com>
2021-04-06 13:30:23 +0300

    fixed function print function sstream added

--- src/Function.cpp
+++ src/Function.cpp
@@ -12,7 +12,7 @@ void Function::print() const{
 void Function::print(double value) const {
 
 	std::ostringstream temp;
-	temp  << std::noshowpoint<<value;
+	temp << std::setprecision(2) << std::fixed << std::noshowpoint << value;
 	std::cout << assign(temp.str());
 
 	//std::cout << assign(std::to_string(value).substr(0, std::to_string(value).find('.') + 3));
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <Function_list.h>
 #pragma once
 int main() {
-	std::cout << std::fixed << std::setprecision(4) <<std::noshowpoint;
+
+	std::cout << std::fixed << std::setprecision(2) <<std::noshowpoint;
 	auto l = Function_list();
 	l.run();
 	
